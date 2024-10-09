51e3c52402751502e0df3045ddaafedfa63d2cd3
adminkaran123 <34962339+adminkaran123@users.noreply.github.com>
2024-10-09 17:26:33 +0530

    my app screen added

--- apps/web/src/app/(private)/(panel)/page.tsx
+++ apps/web/src/app/(private)/(panel)/page.tsx
@@ -18,36 +18,13 @@ export default async function Home() {
 			<div className="flex justify-between">
 				<div>
 					<h1 className='text-2xl font-bold tracking-tight'>
-						Apps
+						Home
 					</h1>
 					<p className='text-muted-foreground'>
 						Here&apos;s a list of your apps!
 					</p>
 				</div>
-				<div className='my-4 flex items-end justify-between sm:my-0 sm:items-center'>
-					<div className='flex flex-col gap-4 sm:my-4 sm:flex-row'>
-					</div>
-					<form className="flex justify-end mb-4">
-						<Button
-							formAction={onCreate}
-							variant="outline"
-							size="sm"
-							className="flex items-center space-x-1 text-green-500 border-green-500 hover:bg-green-100"
-						>
-							Build New App
-						</Button>
-					</form>
 				</div>
-			</div>
-			<Separator className='shadow w-full mt-3'/>
-			<ul className='faded-bottom no-scrollbar grid gap-4 overflow-auto pb-16 pt-4 md:grid-cols-2 lg:grid-cols-3'>
-				{
-					leads.map((lead: any) => (
-						<li key={lead.id}><AppCard id={lead.id} name={lead.name} description={lead.description}
-						                           analytics={{impressions: 0, used: 0}}/></li>
-					))
-				}
-			</ul>
 		</div>
 	);
 }
--- packages/ui/src/components/AppCard.tsx
+++ packages/ui/src/components/AppCard.tsx
@@ -1,59 +1,100 @@
 import React from "react";
 import { Button } from "./ui/button";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "./ui/card";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
+import Image from "next/image";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 
 interface Props {
-	id: string,
-	name: string,
-	description: string,
-	analytics?: {
-		impressions: number,
-		used: number,
-	}
+  id: string;
+  name: string;
+  description: string;
+  analytics?: {
+    impressions: number;
+    used: number;
+  };
 }
 
-export default function AppCard({id, name, description, analytics}: Props) {
-	return (
-		<Card className="p-4">
-			<CardHeader className="flex flex-row justify-between items-start">
-				<div>
-					<CardTitle>{name}</CardTitle>
-					<CardDescription>
-						{description}
-					</CardDescription>
-				</div>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="outline" className="flex flex-row items-center justify-between">
-							<span>Actions</span>
-							<ChevronDown className="h-4 w-4  ml-5"/>
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<div className="flex flex-col p-1">
-							<Link
-								href={`/builder/${id}`}
-								className="p-1 hover:cursor hover:bg-gray-100"
-							>
-								Edit
-							</Link>
-						</div>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</CardHeader>
-			<CardContent className="flex flex-row">
-				<div className="text-center">
-					<p className="text-sm font-medium">Impression</p>
-					<p className="text-lg font-bold">{analytics?.impressions || 0}</p>
-				</div>
-				<div className="text-center">
-					<p className="text-sm font-medium">Usage Count</p>
-					<p className="text-lg font-bold">{analytics?.used || 0}</p>
-				</div>
-			</CardContent>
-		</Card>
-	);
+export default function AppCard({ id, name, description, analytics }: Props) {
+  return (
+    <Card className="p-0 ">
+      <CardHeader className="flex">
+        <div className="flex justify-between items-start">
+          <div className="flex">
+            <div className="flex-shrink-0">
+              <img
+                className="w-[60px] h-[60px] rounded-full"
+                src="https://picsum.photos/seed/picsum/100/300"
+                alt="Neil image"
+                width={100}
+                height={100}
+              />
+			  <CardTitle className="text-md truncate pt-2">{name}</CardTitle>
+			  <CardDescription className="text-sm truncate">
+				{description}
+			  </CardDescription>
+            </div>
+
+            
+          </div>
+          <div className="inline-flex items-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="flex flex-row items-center justify-between"
+                >
+                  <span>Actions</span>
+                  <ChevronDown className="h-4 w-4 ml-2" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <div className="flex flex-col p-1">
+                  <Link
+                    href={`/builder/${id}`}
+                    className="p-1 hover:cursor hover:bg-gray-100"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="px-0 bg-gray-200">
+	<div className="flex items-center justify-center mb-4">
+<div className="w-full h-px bg-gray-300 dark:bg-gray-600" />
+</div>
+
+        <dl className="grid max-w-screen-xl grid-cols-2">
+          
+          <div className="flex flex-col items-center justify-center" >
+            <dt className="text-2xl font-extrabold">
+              {analytics?.impressions || 0}
+            </dt>
+            <dd className="text-gray-500 dark:text-gray-400">Impressions</dd>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <dt className="mb-2 text-2xl font-extrabold">
+              {analytics?.used || 0}
+            </dt>
+            <dd className="text-gray-500 dark:text-gray-400">Usage Count</dd>
+          </div>
+        </dl>
+      </CardContent>
+    </Card>
+  );
 }
