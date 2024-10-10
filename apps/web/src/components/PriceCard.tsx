6cc8f57f96d072ba0a7d09803244d4307006b0d1
adminkaran123 <34962339+adminkaran123@users.noreply.github.com>
2024-10-10 19:41:46 +0530

    new price page

--- apps/web/src/components/PriceCard.tsx
+++ apps/web/src/components/PriceCard.tsx
@@ -1,65 +1,65 @@
+"use client"
+import { useState } from "react";
+import Icon from "@smartleadmagnet/ui/components/icon";
+import { Button } from "@smartleadmagnet/ui/components/ui/button";
+import { Switch } from "@smartleadmagnet/ui/components/ui/switch";
 
-interface PriceCardProps {
-  title: string;
-  description: string;
-  price: string;
-  duration: string;
-  features: { text: string; emphasis?: string }[];
-  buttonText: string;
-  onButtonClick: () => void;
-}
+const PriceCard: React.FC = () => {
+  const [isMonthly, setIsMonthly] = useState(true);
 
-const PriceCard: React.FC<PriceCardProps> = ({
-  title,
-  description,
-  price,
-  duration,
-  features,
-  buttonText,
-  onButtonClick
-}) => {
+  const handleSwitchChange = () => {
+    setIsMonthly(!isMonthly);
+  };
+
+  const listItems = [
+    "Live chat and email",
+    "Fully managed program",
+    "Experienced team members",
+    "Step by step working process",
+    "Re-evaluation project management",
+  ];
+
   return (
-    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-      <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
-      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-        {description}
-      </p>
-      <div className="flex justify-center items-baseline my-8">
-        <span className="mr-2 text-5xl font-extrabold">{price}</span>
-        <span className="text-gray-500 dark:text-gray-400">{duration}</span>
+    <>
+      <div className="flex items-center justify-center mb-8">
+        <p className="text-sm text-gray-500 mr-2">Yearly</p>
+        <Switch checked={isMonthly} onCheckedChange={handleSwitchChange} />
+        <p className="ml-2 text-sm text-gray-500">Monthly</p>
       </div>
-      <ul role="list" className="mb-8 space-y-4 text-left">
-        {features.map((feature, index) => (
-          <li key={index} className="flex items-center space-x-3">
-            <svg
-              className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <span>
-              {feature.text}{" "}
-              {feature.emphasis && (
-                <span className="font-semibold">{feature.emphasis}</span>
-              )}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <button
-        onClick={onButtonClick}
-        className="text-white btn-primary bg-primary rounded-lg px-6 py-3"
-      >
-        {buttonText}
-      </button>
+
+    <div className="border w-full max-w-[1000px] mx-auto flex w-full">
+      <div className="p-[70px] border-r flex-1">
+        <span className="text-lg font-bold text-gray-700">For business</span>
+        <h2 className="text-4xl font-semibold mt-2">
+        <span className="text-gray-800 text-5xl dynamic-value" data-yearly="$24.99" data-monthly="$30.00">
+          {isMonthly ? "$30" : "$24"}
+        </span>
+        <span className="text-lg text-gray-600">/Per Month</span>
+        </h2>
+        <p className="text-gray-600 mt-2">
+        One way to determine how much money you need is to do a break-even analysis.
+        </p>
+      </div>
+      <div className="p-[70px] flex-1">
+        <h3 className="text-xl font-semibold text-gray-700 mb-4">That includes:</h3>
+        <ul className="space-y-4 mb-10">
+            {listItems.map((item, index) => (
+            <li key={index} className="flex items-center">
+                <Icon name="list_icon" color="cyan"  />
+                <span className="ml-4 text-gray-700 font-bold">{item}</span>
+            </li>
+            ))}
+        </ul>
+
+        <div className="mt-4">
+        <Button className="w-full btn-primary">
+          <span>Choose Plan</span>
+        </Button>
+        </div>
+      </div>
     </div>
-  )
-}
+    </>
+  );
+};
 
 export default PriceCard;
