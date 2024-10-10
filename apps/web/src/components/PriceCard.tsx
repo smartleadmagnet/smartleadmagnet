998ae6beff4f26bc18e274fd6330325d0e581239
adminkaran123 <34962339+adminkaran123@users.noreply.github.com>
2024-10-10 19:43:13 +0530

    responasive

--- apps/web/src/components/PriceCard.tsx
+++ apps/web/src/components/PriceCard.tsx
@@ -27,37 +27,37 @@ const PriceCard: React.FC = () => {
         <p className="ml-2 text-sm text-gray-500">Monthly</p>
       </div>
 
-    <div className="border w-full max-w-[1000px] mx-auto flex w-full">
-      <div className="p-[70px] border-r flex-1">
-        <span className="text-lg font-bold text-gray-700">For business</span>
-        <h2 className="text-4xl font-semibold mt-2">
-        <span className="text-gray-800 text-5xl dynamic-value" data-yearly="$24.99" data-monthly="$30.00">
-          {isMonthly ? "$30" : "$24"}
-        </span>
-        <span className="text-lg text-gray-600">/Per Month</span>
-        </h2>
-        <p className="text-gray-600 mt-2">
-        One way to determine how much money you need is to do a break-even analysis.
-        </p>
-      </div>
-      <div className="p-[70px] flex-1">
-        <h3 className="text-xl font-semibold text-gray-700 mb-4">That includes:</h3>
-        <ul className="space-y-4 mb-10">
+      <div className="border w-full max-w-[1000px] mx-auto flex flex-col md:flex-row">
+        <div className="p-[70px] border-b md:border-b-0 md:border-r flex-1">
+          <span className="text-lg font-bold text-gray-700">For business</span>
+          <h2 className="text-4xl font-semibold mt-2">
+            <span className="text-gray-800 text-5xl dynamic-value" data-yearly="$24.99" data-monthly="$30.00">
+              {isMonthly ? "$30" : "$24"}
+            </span>
+            <span className="text-lg text-gray-600">/Per Month</span>
+          </h2>
+          <p className="text-gray-600 mt-2">
+            One way to determine how much money you need is to do a break-even analysis.
+          </p>
+        </div>
+        <div className="p-[70px] flex-1">
+          <h3 className="text-xl font-semibold text-gray-700 mb-4">That includes:</h3>
+          <ul className="space-y-4 mb-10">
             {listItems.map((item, index) => (
-            <li key={index} className="flex items-center">
-                <Icon name="list_icon" color="cyan"  />
+              <li key={index} className="flex items-center">
+                <Icon name="list_icon" color="cyan" />
                 <span className="ml-4 text-gray-700 font-bold">{item}</span>
-            </li>
+              </li>
             ))}
-        </ul>
+          </ul>
 
-        <div className="mt-4">
-        <Button className="w-full btn-primary">
-          <span>Choose Plan</span>
-        </Button>
+          <div className="mt-4">
+            <Button className="w-full btn-primary">
+              <span>Choose Plan</span>
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
