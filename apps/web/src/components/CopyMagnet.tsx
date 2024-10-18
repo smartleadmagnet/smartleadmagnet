953da140b7267ad6e8e01c64276b6ec960ac5ae1
adminkaran123 <34962339+adminkaran123@users.noreply.github.com>
2024-10-18 13:45:02 +0530

    code

--- apps/web/src/components/CopyMagnet.tsx
+++ apps/web/src/components/CopyMagnet.tsx
@@ -10,7 +10,7 @@ export default function CopyMagnet({ id,onClone }: { id: string,onClone:()=>void
     <>
         <button onClick={onClone} className="flex items-center p-1 hover:bg-gray-100">
             <CopyIcon className="pl-1 mr-2 h-4 w-4" />
-            Clone
+            Clone Magnet
         </button>
     </>
   );
