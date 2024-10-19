0a1be60fafbe3a9538c387d98204864787f741d8
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-19 12:35:23 +0530

    "Improved user experience with credit-based pricing and additional features."

--- apps/web/src/app/share/[id]/page.tsx
+++ apps/web/src/app/share/[id]/page.tsx
@@ -1,8 +1,13 @@
-import { getLeadMagnetById, updateLeadMagnetImpressions } from "@smartleadmagnet/services";
+import { getLeadMagnetById, getUserById, updateLeadMagnetImpressions } from "@smartleadmagnet/services";
 import BuilderContainer from "@/app/share/[id]/BuilderContainer";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const leadMagnet = await getLeadMagnetById(params.id);
+  const user = await getUserById(leadMagnet?.userId);
+  if (!user.Credit || (user?.Credit?.total || 0) > (user?.Credit?.used || 0)) {
+    return <div>Not enough credits</div>;
+  }
+
   await updateLeadMagnetImpressions(params.id);
 
   return <BuilderContainer leadMagnet={leadMagnet} />;
