e556fcbec69c1d9c60e8a3e2a102a68ef9fb374a
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-23 01:46:25 +0530

    Change API rewrites

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -11,10 +11,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/blog",
-        destination: "https://silver-caribou-278976.hostingersite.com",
-      },
-      {
         source: "/blog/:path*",
         destination: "https://silver-caribou-278976.hostingersite.com/blog/:path*",
       },
