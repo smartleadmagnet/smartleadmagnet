40255437fa25c5277a3a25db69746b19a62a5eff
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-23 02:10:04 +0530

    Update API rewrites to include new path.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -15,6 +15,10 @@ const nextConfig = {
         destination: "https://silver-caribou-278976.hostingersite.com/blog/:path*",
       },
       {
+        source: "/wp-admin/:path*",
+        destination: "https://silver-caribou-278976.hostingersite.com/wp-admin/:path*",
+      },
+      {
         source: "/wp-json/aioseo/:path*",
         destination: "https://silver-caribou-278976.hostingersite.com/wp-json/aioseo/:path*",
       },
