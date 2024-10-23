d6788d5107a6bc0d9d84c38afb2e7147c9dea46f
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-24 02:24:31 +0530

    Redirect to home page if lead magnet not found.

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      log: ["query", "info", "warn", "error"],
+    });
   }
   prisma = global.prisma;
 }
