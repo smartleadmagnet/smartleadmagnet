f140e4ca1f166020838ab8850501d2715ea92c5b
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-23 23:32:11 +0530

    Added SEO pages

--- apps/web/src/app/(public)/(panel)/page.tsx
+++ apps/web/src/app/(public)/(panel)/page.tsx
@@ -9,11 +9,26 @@ import CtaSection from "@/components/Homepage/CtaSection";
 import { getSessionUser } from "@/services/user";
 import { redirect } from "next/navigation";
 import getSeo from "@/lib/seo";
+import { WebPageJsonLd, FAQPageJsonLd, ProductJsonLd, OrganizationJsonLd, BreadcrumbJsonLd } from "next-seo";
+import faq from "@/data/faq.json";
+import testimonial from "@/data/testimonial.json";
+import { AggregateRating } from "next-seo/lib/types";
+
+const title = "SmartLeadMagnet: AI Lead Magnets to Boost Website Traffic";
+const description = `Drive more traffic to your website with SmartLeadMagnet’s AI-powered lead magnets. Capture leads, grow your audience, and boost your business easily.`;
+
+const aggRating: AggregateRating = {
+  ratingValue: "4.8",
+  reviewCount: testimonial?.length?.toString(),
+  ratingCount: "120",
+  bestRating: "5",
+  worstRating: "4",
+};
 
 export function generateMetadata() {
   return getSeo({
-    title: "SmartLeadMagnet: AI Lead Magnets to Boost Website Traffic",
-    description: `Drive more traffic to your website with SmartLeadMagnet’s AI-powered lead magnets. Capture leads, grow your audience, and boost your business easily.`,
+    title,
+    description,
   });
 }
 
@@ -24,6 +39,77 @@ export default async function Home() {
   }
   return (
     <div className="flex size-full  flex-col">
+      <WebPageJsonLd
+        useAppDir
+        id="https://smartleadmagnet.com/"
+        url="https://smartleadmagnet.com/"
+        title={<title></title>}
+        description={description}
+        images={[
+          "https://smartleadmagnet.com/og-image.png",
+          "https://smartleadmagnet.com/images/logo/logo.png",
+          "https://smartleadmagnet.com/lead-magnet-01.png",
+          "https://smartleadmagnet.com/lead-magnet-02.png",
+        ]}
+        inLanguage="en-US"
+      />
+      <FAQPageJsonLd
+        useAppDir
+        mainEntity={faq.map((item) => ({
+          questionName: item.question,
+          acceptedAnswerText: item.answer,
+        }))}
+      />
+      <OrganizationJsonLd
+        useAppDir
+        id="https://smartleadmagnet.com/"
+        url="https://smartleadmagnet.com/"
+        name="SmartLeadMagnet"
+        logo="https://smartleadmagnet.com/assets/Logo.png"
+        sameAs={[
+          "https://www.facebook.com/SmartLeadMagnet",
+          "https://www.twitter.com/SmartLeadMagnet",
+          "https://www.linkedin.com/company/SmartLeadMagnet",
+        ]}
+      />
+      <ProductJsonLd
+        useAppDir
+        description={description}
+        id={title}
+        productName="SmartLeadMagnet"
+        images={[
+          "https://smartleadmagnet.com/og-image.png",
+          "https://smartleadmagnet.com/assets/Logo.png",
+          "https://smartleadmagnet.com/features/drag-and-drop.png",
+          "https://smartleadmagnet.com/features/ai-powered.png",
+          "https://smartleadmagnet.com/features/lead-capture.png",
+          "https://smartleadmagnet.com/assets/pricing/01.png",
+          "https://smartleadmagnet.com/assets/pricing/02.png",
+          "https://smartleadmagnet.com/assets/pricing/03.png",
+        ]}
+        reviews={testimonial?.map((t) => ({
+          author: t.name,
+          datePublished: "2024-01-06T03:37:40Z",
+          reviewBody: t.description,
+          reviewRating: {
+            bestRating: "5",
+            ratingValue: "5",
+            worstRating: "1",
+          },
+        }))}
+        aggregateRating={aggRating}
+      />
+      <BreadcrumbJsonLd
+        useAppDir
+        itemListElements={[
+          {
+            position: 1,
+            name: "Home",
+            item: "https://smartleadmagnet.com/",
+          },
+        ]}
+      />
+
       <HeroSection />
       {/*<BrandSection />*/}
       <FeatureSection />
--- apps/web/src/components/Homepage/AccordionSection.tsx
+++ apps/web/src/components/Homepage/AccordionSection.tsx
@@ -1,34 +1,18 @@
 import Accordion from "@/components/Accordion";
+import faqs from "@/data/faq.json";
 
 const AccordionSection = () => {
-  const faqs = [
-    {
-      question: "What is DummyLib?",
-      answer:
-        "DummyLib is a fictional open-source library that provides various components built on a CSS framework for quick and easy website development.",
-    },
-    {
-      question: "Is there a design file available?",
-      answer:
-        "Yes, DummyLib comes with a design file, available in popular design software, to help you integrate its components visually before coding.",
-    },
-    {
-      question: "What are the differences between DummyLib and SuperUI?",
-      answer:
-        "DummyLib is free and open source, focusing on individual components, while SuperUI is a premium product offering full-page layouts. Both can be used together for an enhanced development experience.",
-    },
-  ];
-
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-blue-400 py-16">
-      <div className="max-w-[1000px] mx-auto px-4">
+      <div className="mx-auto max-w-[1000px] px-4">
         <div className="flex flex-col">
           {/* Title Section */}
-          <h2 className="mb-4 text-3xl font-bold md:text-5xl text-center text-white">Frequently Asked Questions</h2>
-          <p className="text-xl text-gray-700 md:text-2xl mb-8 max-w-[600px] mx-auto text-center text-white">
-          Explore our FAQs to learn more about how SmartLead Magnet can transform your website to lead genration machine.
+          <h2 className="mb-4 text-center text-3xl font-bold text-white md:text-5xl">Frequently Asked Questions</h2>
+          <p className="mx-auto mb-8 max-w-[600px] text-center text-xl text-gray-700 text-white md:text-2xl">
+            Explore our FAQs to learn more about how SmartLead Magnet can transform your website to lead genration
+            machine.
           </p>
-          
+
           {/* FAQ Section */}
           <Accordion faqs={faqs} />
         </div>
