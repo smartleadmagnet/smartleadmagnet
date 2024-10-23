b1ca3358285bab96eebf5a56d97c668c5fde51b8
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-24 02:33:15 +0530

    Added the query

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -14,11 +14,47 @@ declare global {
 }
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    log: [
+      {
+        emit: "stdout",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient({
-      log: ["query", "info", "warn", "error"],
+      log: [
+        {
+          emit: "stdout",
+          level: "query",
+        },
+        {
+          emit: "stdout",
+          level: "error",
+        },
+        {
+          emit: "stdout",
+          level: "info",
+        },
+        {
+          emit: "stdout",
+          level: "warn",
+        },
+      ],
     });
   }
   prisma = global.prisma;
