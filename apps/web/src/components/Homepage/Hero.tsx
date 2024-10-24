cd8f86d701e2ccaad171a00dec352891f3f8bbaa
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2024-10-24 14:52:33 +0530

    Added Interactive Demo

--- apps/web/src/components/Homepage/Hero.tsx
+++ apps/web/src/components/Homepage/Hero.tsx
@@ -1,16 +1,15 @@
-import Link from "next/link";
 import Image from "next/image";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="hero-section relative text-center" id="hero">
+    <div className="hero-section relative max-h-screen min-h-screen text-center" id="hero">
       <Image className="hero-shape shape-1" src="/images/home/magnet.svg" alt="image shape" width={100} height={100} />
       <Image className="hero-shape shape-2" src="/images/home/magnify.svg" alt="image shape" width={100} height={100} />
       <Image className="hero-shape shape-3" src="/images/home/pen.svg" alt="image shape" width={100} height={100} />
 
       <div className="container mx-auto px-10">
         <div className="flex flex-wrap">
-          <div className="mx-auto w-full md:w-9/12">
+          <div className="mx-auto w-full">
             <div className="py-12">
               <div className="mb-6">
                 <p className="text-lg text-gray-500">Having trouble generating leads from your website?</p>
@@ -24,32 +23,47 @@ const HeroSection: React.FC = () => {
                 </h1>
 
                 <p className="mt-4 text-lg">
-                Transform your website into a powerful lead generation machine! With simple steps, you can create a lead magnet that will help you generate more leads and grow your business.
+                  Transform your website into a powerful lead generation machine! With simple steps, you can create a
+                  lead magnet that will help you generate more leads and grow your business.
                 </p>
               </div>
-              <div className="mb-10 flex flex-col justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <Link
-                  className="rounded-lg bg-cyan-500 px-6 py-3 text-center text-xl font-bold text-white hover:bg-cyan-600 sm:px-8 sm:py-4 sm:text-2xl"
-                  href="/app"
-                >
-                  Build Magnet Now
-                </Link>
-                <Link
-                  href="#"
-                  className="rounded-lg border-2 border-cyan-500 px-6 py-3 text-center text-xl font-bold text-cyan-500 hover:bg-cyan-500 hover:text-white sm:px-8 sm:py-4 sm:text-2xl"
-                >
-                  See How It Works
-                </Link>
+              <div
+                style={{
+                  position: "relative",
+                  boxSizing: "content-box",
+                  maxHeight: "80vh",
+                  height: "80vh",
+                  width: "100%",
+                  aspectRatio: "1.9834710743801653",
+                  padding: "5px 0",
+                }}
+              >
+                <iframe
+                  src="https://app.supademo.com/embed/cm2n1nipb05s6k69dpnd4bolh?embed_v=2"
+                  loading="lazy"
+                  title="Smartleadmagnet Demo"
+                  allow="clipboard-write"
+                  frameBorder="0"
+                  webkitallowfullscreen="true"
+                  mozallowfullscreen="true"
+                  allowFullScreen
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                ></iframe>
               </div>
-
-              <Image
-                className="w-full"
-                src="/images/home/hero-image.png"
-                alt="alternative text"
-                layout="responsive"
-                width={700}
-                height={475}
-              />
+              {/*<Image*/}
+              {/*  className="w-full"*/}
+              {/*  src="/images/home/hero-image.png"*/}
+              {/*  alt="alternative text"*/}
+              {/*  layout="responsive"*/}
+              {/*  width={700}*/}
+              {/*  height={475}*/}
+              {/*/>*/}
             </div>
           </div>
         </div>
